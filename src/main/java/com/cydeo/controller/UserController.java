0286fc02604dc4307d272bf0de39c72b923fd80a
olegtsetnar <oleg.cetnar@gmail.com>
2023-01-18 16:35:51 -0800

    ticket-4

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -10,6 +10,8 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,6 +35,16 @@ public class UserController {
         model.addAttribute("users", userService.findAll());
 
         return "user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute ("user") UserDTO user, Model model) {
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+
+        userService.save(user);
+       model.addAttribute("users", userService.findAll());
+        return "/user/create";
     }
 
 
