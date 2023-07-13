e0b7d618c03dd5c4ae06b71ad5509c433d6bb1ef
olegtsetnar <oleg.cetnar@gmail.com>
2023-07-13 10:13:58 -0700

    project ORM begin

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -10,7 +10,9 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class BaseEntity {
+
     private Long id;
     private LocalDateTime insertDateTime;
     private LocalDateTime lastUpdateDateTime;
